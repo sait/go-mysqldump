f701cd3b005f69e9bd8d8a160f22b4805926a9c4
Sergio Contreras <sergio@saitenlinea.com>
2018-08-07 11:22:22 -0700

    The sentences are now separated by Line break

--- dump.go
+++ dump.go
@@ -25,46 +25,47 @@ type dump struct {
 
 const version = "0.2.2"
 
-const tmpl = `-- Go SQL Dump {{ .DumpVersion }}
---
--- ------------------------------------------------------
--- Server version	{{ .ServerVersion }}
-
+const tmpl = `
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+
 /*!40101 SET NAMES utf8 */;
+
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+
 /*!40103 SET TIME_ZONE='+00:00' */;
+
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
 {{range .Tables}}
---
--- Table structure for table {{ .Name }}
---
-
 DROP TABLE IF EXISTS {{ .Name }};
+
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
+
 /*!40101 SET character_set_client = utf8 */;
+
 {{ .SQL }};
+
 /*!40101 SET character_set_client = @saved_cs_client */;
---
--- Dumping data for table {{ .Name }}
---
 
 LOCK TABLES {{ .Name }} WRITE;
+
 /*!40000 ALTER TABLE {{ .Name }} DISABLE KEYS */;
 {{ if .Values }}
 INSERT INTO {{ .Name }} VALUES {{ .Values }};
 {{ end }}
 /*!40000 ALTER TABLE {{ .Name }} ENABLE KEYS */;
+
 UNLOCK TABLES;
 {{ end }}
--- Dump completed on {{ .CompleteTime }}
 `
 
 // Creates a MYSQL Dump based on the options supplied through the dumper.
